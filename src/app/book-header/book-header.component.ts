bdc03d81a38bffd00205330a081fc0d77b0694a2
Giovanni <giovanni.sokenou2105@gmail.com>
2024-08-06 18:41:37 +0200

    update emit et props

--- src/app/book-header/book-header.component.ts
+++ src/app/book-header/book-header.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-book-header',
   templateUrl: './book-header.component.html',
-  styleUrl: './book-header.component.css'
+  styleUrl: './book-header.component.css',
 })
 export class BookHeaderComponent {
-logo : string ="../assets/logoipsum-317.svg";
-affiche:boolean = false;
-toogleForm(){
- this.affiche=!this.affiche;
-}
-
+  logo: string = '../assets/logoipsum-317.svg';
+  @Output() passValue = new EventEmitter<boolean>();
+  affiche: boolean = false;
+  toogleForm() {
+    this.affiche = !this.affiche;
+    this.passValue.emit(this.affiche);
+  }
 }
--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrl: './book.component.css',
 })
 export class BookComponent {
-
+  getChildInfo !:boolean;
+  recupValue(val:boolean) {
+    console.log('Valeur recuperer =', val);
+    this.getChildInfo=val;
+  }
 }
